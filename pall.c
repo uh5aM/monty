a02df26a4fce68776bb0a8ceae37659a36a850f5
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-12-13 13:38:59 -0800

    all in the additio

--- pall.c
+++ pall.c
@@ -1,23 +1,21 @@
 #include "monty.h"
 /**
- * @f_pall: print the stacj
- *@head: head of the stack
- @counter: no used
- return:  nothing 
- * 
- */
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
 void f_pall(stack_t **head, unsigned int counter)
 {
-  stack_t *h;
-  (void) counter;
+	stack_t *h;
+	(void)counter;
 
-  h = *head;
-  if (h == NULL)
-  return;
-
-  while (h)
-  {
-    printf("%d\n", h->n);
-    h = h->next;
-  }
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
